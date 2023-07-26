6e8bd0d521168fdd8dd1668f68b36f84601ad1e1
Rogier Ottens <rogier.ottens@wundermanthompson.com>
2023-07-26 11:22:02 +0200

    main start of Nextjs 13 and chakra UI

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import HeroSection from "@/components/HeroSection";
+import Features from "@/components/Features";
+import Testimonials from "@/components/Testimonials";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <Features />
+      <Testimonials />
+    </>
+  );
+}
